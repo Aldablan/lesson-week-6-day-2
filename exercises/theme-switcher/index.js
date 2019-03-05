22ebaa1d05b8944e9a0701474f1b8ef04a2f4dc6
Aldablan <46870563+Aldablan@users.noreply.github.com>
2019-03-05 21:48:04 +0300

    Homework
    it was easier than the last one!

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -1,13 +1,28 @@
 // click event for gray button
-
+$("#grayButton").on("click", switchGray);
 // click event for white button
+$("#whiteButton").on("click", switchWhite);
+
+
 
 function switchGray() {
     // change background to gray
     // change font to white
-}
+
+
+      $("body").css("background", "gray");
+      $("h1").css("color", "white");
+      $("p").css("color", "white");
+      }
+
+
 
 function switchWhite() {
     // change background to white
     // change font to black
-}
+
+
+    $("body").css("background", "white");
+    $("h1").css("color", "black");
+    $("p").css("color", "black");
+    }
